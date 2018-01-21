ceec304ae8230a041d8e52aa93c69896ed080a34
Chirag Bohet <bohet.chirag@gmail.com>
2018-01-21 11:24:18 +0530

    Create reverse using stack.cpp

---
+++ reverse using stack.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+
+stack<int>myStack;
+
+void input(int elements){
+	
+	int item;
+	cout<<"Enter element ["<<elements<<"] : ";
+	cin>>item;
+	myStack.push(item);
+	
+	}
+	
+void reverse(){
+	
+	cout<<myStack.top()<<"\n";
+	myStack.pop();
+	
+	}
+	
+int main(){
+	
+
+	int elements;
+	cout<< "Enter No of Elements to add : ";
+	cin>>elements;
+	
+	for(int i=1;i<=elements;i++){
+		
+		input(i);
+	}
+	
+	cout<<"Popping out elements from the stack...\n";
+	
+	while(!myStack.empty()){
+		
+		reverse();
+	}
+	
+	
+	
+	
+}
